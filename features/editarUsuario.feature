f9bac32d02a06e43a0cb986ef9b71f84a764f520
andymehi <andymehi@hotmail.com>
2019-05-31 21:39:58 -0400

    Editar nombre del usuario

--- features/editarUsuario.feature
+++ features/editarUsuario.feature
@@ -8,13 +8,11 @@ Scenario: Editar nombre del usuario
   And Ingreso "carito" como usuario
   And Ingreso "carito123" como contraseña
   And presiono el boton "Ingresar" 
-  When estoy en la pagina de eventos
-  And presiono el boton con el nombre "Carolina Villalobos"
+  When estoy en la pagina de eventos y presiono el boton con el nombre "Carolina Villalobos"
   And presiono la opcion "Modificar mis datos"
   And Ingreso "Carolina Andrea" en el campo nombre
   And Ingreso "Villalobos Montaño" en el campo apellido
   And Presiono el boton "Editar"
-  Then El sistema regresa a la pagina de eventos
-  And El sistema muestra el mensaje "Tu cuenta se ha actualizado"
+  Then El sistema muestra el mensaje "Tu cuenta se ha actualizado"
 
 
--- features/step_definitions/editarUsuario.rb
+++ features/step_definitions/editarUsuario.rb
@@ -14,34 +14,26 @@ Given("presiono el boton {string}") do |ingresar|
     click_button(ingresar)
 end
 
-When("estoy en la pagina de eventos") do
-    page.current_url('http://docs-academicos.herokuapp.com/')
-end
-
-When("presiono el boton con el nombre {string}") do |nombre|
+When("estoy en la pagina de eventos y presiono el boton con el nombre {string}") do |nombre|
     click_link nombre
 end
 
-When("presiono la opcion {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+When("presiono la opcion {string}") do |opcion|
+    click_link opcion
 end
 
-When("Ingreso {string} en el campo nombre") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+When("Ingreso {string} en el campo nombre") do |nombre|
+    fill_in 'user[name]', :with => nombre
 end
 
-When("Ingreso {string} en el campo apellido") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+When("Ingreso {string} en el campo apellido") do |apellido|
+    fill_in 'user[lastname]', :with => apellido
 end
 
-When("Presiono el boton {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+When("Presiono el boton {string}") do |editar|
+    click_button(editar)
 end
 
-Then("El sistema regresa a la pagina de eventos") do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("El sistema muestra el mensaje {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+Then("El sistema muestra el mensaje {string}") do |mensaje|
+    page.has_content?(mensaje)
 end
