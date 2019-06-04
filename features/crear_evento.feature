287a406cc3ed486c091260493f198371e099b674
Maria de los Angeles Gallardo <maye.gallardoger@gmail.com>
2019-06-04 08:04:19 -0400

    crear evento

--- features/crear_evento.feature
+++ features/crear_evento.feature
@@ -3,18 +3,19 @@ Feature: Crear un evento
   Quiero crear un evento
   Para poder hacer un seguimiento en la revisión de los documentos de estudiantes
 
-
+  @onlyThis
   Scenario: datos validos
     Given Visito la pagina principal
-    And Ingresar "carito" como usuario
-    And Ingresar "carito123" como contraseña
+    And Ingresar "laurita" como usuario
+    And Ingresar "laura123" como contraseña
     And presiono el boton "Ingresar"
-    When estoy en la pagina de eventos y presiono el boton de "Evntos"
-    And presiono la opcion "crear evento"
-    And selecciono "segundo" en el campo de semestre
-    And selecciono "2019" en el campo de anio
-    And Ingresar "investigacion redes" en el campo nombre del evento
+    When estoy en la pagina de eventos y presiono el boton de "Eventos"
+    And presiono la opcion "Crear evento"
+    When estoy en la pagina de nuevo evento y presiono en "Primer"
+    And presiono el boton de "Elige una carrera"
     And selecciono "Ingenieria de sistemas" en el campo de carrera
+    And Ingresar "investigacion redes" en el campo nombre del evento
+    And presiono en el campo de "Fecha límite de recepción de documentos"
     And selecciono "2019-06-25" en el campo fecha limite
     And Presiono el boton "registrar evento"
     Then El sistema muestra la lista de eventos
--- features/step_definitions/crear_evento_steps.rb
+++ features/step_definitions/crear_evento_steps.rb
@@ -2,19 +2,32 @@ Given("Visito la pagina principal") do
   visit 'http://docs-academicos.herokuapp.com/'
 end
 
-When("estoy en la pagina de eventos y presiono el boton de {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Given("Ingresar {string} como usuario") do |username|
+  fill_in 'user[login]', :with => username
 end
 
-When("selecciono {string} en el campo de semestre") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Given("Ingresar {string} como contraseña") do |password|
+  fill_in 'user[password]', :with => password
 end
 
-When("selecciono {string} en el campo de anio") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Given("presiono el boton {string}") do |ingresar|
+  click_button(ingresar)
 end
 
-When("Ingresar {string} en el campo nombre del evento") do |string|
+When("estoy en la pagina de eventos y presiono el boton de {string}") do |eventos|
+  click_on eventos
+end
+
+When("presiono la opcion {string}") do |crear|
+  click_on crear
+end
+
+When("estoy en la pagina de nuevo evento y presiono en {string}") do |primer|
+  click_on(primer)
+  sleep 3
+end
+
+When("presiono el boton de {string}") do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
@@ -22,7 +35,19 @@ When("selecciono {string} en el campo de carrera") do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
+When("Ingresar {string} en el campo nombre del evento") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("presiono en el campo de {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
 When("selecciono {string} en el campo fecha limite") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("Presiono el boton {string}") do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
