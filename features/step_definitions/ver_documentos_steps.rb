ebc609b981b0af7fc216c5138d8319a97e649b2e
Julia1297 <valita129@gmail.com>
2019-06-10 20:55:34 -0400

    Ver documento como estudiante

--- features/step_definitions/ver_documentos_steps.rb
+++ features/step_definitions/ver_documentos_steps.rb
@@ -5,4 +5,32 @@ end
   
 Then("se muestra en pantalla una tabla con Mis Documentos") do 
     page.has_text?('Mis Documentos')
+    expect(page).to have_css 'table'
+    expect(page).to have_text 'No.'
+    expect(page).to have_text 'Título'
+
 end
+When("presiono el documento {string}") do |nombre|
+    click_link(nombre)
+end
+
+Then("se muestra en pantalla la información del documento") do |table|
+    expect(page).to have_css 'table'
+
+    data = table.rows_hash
+    data.each_pair do |key, value|
+      case key
+        when "Tipo de documento:"
+            expect(page).to have_text value
+        when "Título:"
+            expect(page).to have_text value
+        when "Autor:"
+            expect(page).to have_text value
+        when "Link Google Docs:"
+            expect(page).to have_text value
+        when "Presentado:"
+            expect(page).to have_text value
+      end
+    end
+end
+
--- features/ver_documentos.feature
+++ features/ver_documentos.feature
@@ -8,6 +8,17 @@ Background:
     And ingreso con mi usuario y contraseña de estudiante
     And presiono el boton "Ingresar"
 
-Scenario: Ver documentos     
+Scenario: Ver mis documentos     
   When presiono la opcion "Mis Documentos"
   Then se muestra en pantalla una tabla con Mis Documentos
+
+Scenario: Ver un documento     
+  When presiono la opcion "Mis Documentos"
+  And presiono el documento "Caratula"
+  Then se muestra en pantalla la información del documento
+      |Tipo de documento: 	  | Trabajo Dirigido          |
+      |Título: 	              | Caratula                  |
+      |Autor: 	              | Julia Gutierrez           |
+      |Link Google Docs:      |	https://drive.google.com/file/d/1q1mL037QB3MtQ5er45h8fxly5TE2aDWT/view?usp=sharing  |
+      |Presentado:	          | 09 Abr, 2019 - 22:21      |
+
